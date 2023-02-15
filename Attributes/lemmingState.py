538cbd56ff1531c879f8a44e1103ee6b087b6709
Kuba <qbab.net@gmail.com>
2023-02-15 12:21:44 +0100

    do akceptacji

--- Attributes/lemmingState.py
+++ Attributes/lemmingState.py
@@ -1,6 +1,14 @@
 class LemmingState:
     Walk = 1
     Fall = 2
+    Climb = 3
+    Dig = 4
+    Stop = 5
 class LemmingDirection:
     Left = False
     Right = True
+class LemmingAbility:
+    Floater = 1
+    Digger = 2
+    Climber = 3
+    Stopper = 4
--- BackgroundService.py
+++ BackgroundService.py
@@ -1,5 +1,5 @@
 from Models.Model import Model
-from Attributes.lemmingState import LemmingState
+from Attributes.lemmingState import LemmingState,LemmingDirection
 from Models.Lemming import Lemming
 
 class BackgroundService:
@@ -11,33 +11,38 @@ class BackgroundService:
         if (lemming.state == LemmingState.Walk):
             if (lemming.direction == LemmingDirection.Right):
                 newRightXEdge = lemming.position[0] + lemming.size[0]
+                # najpierw sprawdzanie podłogi
+                if (self.model.currentLevel.map[lemming.position[0]][lemming.position[1]+lemming.size[1]] == False):
+                    lemming.state= LemmingState.Fall
+                else:
+                    lemming.state = LemmingState.Walk
+                
+                # później sprawdzanie ścian
                 if (self.model.currentLevel.map[newRightXEdge][lemming.position[1]] == False): #nie ma ściany
                     lemming.position[0] = lemming.position[0] + 1
                 else:
                     lemming.direction = LemmingDirection.Left
-                    #     O
-                    #     O
-                    #     
-                    #     O
-                    #xxxxXO
-                    #xxxx
-                    #xxxx
-                    #xxxx
-                if (self.model.currentLevel.map[newRightXEdge][lemming.position[1]+lemming.size[1]] == False):
-                    lemming.state= LemmingState.Fall
-                    print(lemming.position[0],lemming.position[1])
-                    print(map)
-
+                    
             if (lemming.state == LemmingState.Walk) and (lemming.direction == LemmingDirection.Left):
                 newLeftXEdge  = lemming.position[0] - 1
+                if (self.model.currentLevel.map[lemming.position[0]][lemming.position[1]+lemming.size[1]] == False):
+                    lemming.state= LemmingState.Fall
+                else:
+                    lemming.state = LemmingState.Walk
+                
+                print(self.model.currentLevel.map[newLeftXEdge][lemming.position[1]])
                 if (self.model.currentLevel.map[newLeftXEdge][lemming.position[1]] == False):
                     lemming.position[0] = lemming.position[0] - 1
                 else:
-                    lemming.state = LemmingState.WalkRight
-        
+                    lemming.direction = LemmingDirection.Right
+            print("state :",lemming.state)
 
         if (lemming.state == LemmingState.Fall):
-            lemming.position[1] = lemming.position[1] + 1
+            newRightXEdge = lemming.position[0] + lemming.size[0]
+            if (self.model.currentLevel.map[lemming.position[0]][lemming.position[1]+lemming.size[1]] == True):
+                lemming.state = LemmingState.Walk
+            else:
+                lemming.position[1] = lemming.position[1] + 1
 
 
     def __updateLevelModel(self):
@@ -46,4 +51,5 @@ class BackgroundService:
     
     def updateModel(self):
         self.__updateLevelModel()
+    
 
